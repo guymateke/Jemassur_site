09887e0628b5d5f80f2a49cc3ed6f8ab319c3afb
GUILLAUME <guymateke69@gmail.com>
2025-03-24 15:40:42 +0000

    changement hero 5eme commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,3 +213,14 @@
   });
 
 })();
+// -- Swiper.js Script 
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var swiper = new Swiper('.hero-slider', {
+            loop: true,
+            autoplay: { delay: 4000, disableOnInteraction: false },
+            pagination: { el: '.swiper-pagination', clickable: true },
+            navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+            effect: "fade"
+        });
+    });
